03a254482e860eb90de29468bb31a3b324a7641a
QuangAnh <20521059@gm.uit.edu.vn>
2023-12-03 21:21:47 +0700

    commit many time :<<<

--- src/index.js
+++ src/index.js
@@ -5,15 +5,18 @@ const routes = require('./routes')
 const cors = require('cors');
 const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser')
+
+dotenv.config()
+
 const app = express()
 const port = process.env.PORT || 3001
-dotenv.config()
+
 app.use(cors())
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(bodyParser.json())
 app.use(cookieParser())
-
+dotenv.config()
 routes(app);
 
 mongoose.connect('mongodb+srv://ddqa2208:qa123456789@data-new.s2021bv.mongodb.net/?retryWrites=true&w=majority')
